550d9840044e9a3a97d5a1fccc9d900676232f60
astral0525 <astral0525@gmail.com>
2024-01-08 10:34:04 +0900

    C:/Program Files/Git/article/doWrite

--- src/main/java/com/ll/sb20231114/Controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/Controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ArticleController {
@@ -9,6 +10,15 @@ public class ArticleController {
     @GetMapping("/article/write")
     String showWrite(){
         return "article/write";
+    }
+
+    @GetMapping("/article/doWrite")
+    @ResponseBody
+    String doWrite(
+            String title,
+            String body
+    ){
+        return "게시물이 작성되었습니다.";
     }
 
 }
