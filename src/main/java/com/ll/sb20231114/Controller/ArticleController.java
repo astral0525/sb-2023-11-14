bc915a2478c28811da9387dfa0dc3c3d72e8edc2
astral0525 <astral0525@gmail.com>
2024-01-08 11:04:56 +0900

    article array

--- src/main/java/com/ll/sb20231114/Controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/Controller/ArticleController.java
@@ -1,11 +1,18 @@
 package com.ll.sb20231114.Controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class ArticleController {
+    private Article[] articles =  new Article[3];
+    private int articlesLastIndex = -1;
 
     @GetMapping("/article/write")
     String showWrite(){
@@ -14,15 +21,33 @@ public class ArticleController {
 
     @GetMapping("/article/doWrite")
     @ResponseBody
-    String doWrite(
+    Map<String, Object> doWrite(
             String title,
             String body
     ){
-        return "게시물이 작성되었습니다.";
+        Article article = new Article(articlesLastIndex+2, title, body);
+        Map<String, Object> rs = new HashMap<>();
+        rs.put("msg", "%d번 게시물이 작성되었습니다.".formatted(article.getId()));
+        rs.put("data", article);
+
+        articles[++articlesLastIndex] = article;
+        return rs;
+    }
+
+    @GetMapping("/article/getLastArticle")
+    @ResponseBody
+    Article getLastArticle(){
+        return articles[articlesLastIndex];
+    }
+
+    @GetMapping("/article/getArticles")
+    @ResponseBody
+    Article[] getArticles(){
+        return articles;
     }
 
 }
-/*
+
 @AllArgsConstructor
 @Getter
 class Article{
@@ -30,4 +55,3 @@ class Article{
     private String title;
     private String body;
 }
-*/
